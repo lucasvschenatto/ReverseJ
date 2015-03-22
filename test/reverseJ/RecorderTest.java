811365324709cc5e870b764f6945144f581f46e3
Lucas <Lucas@LUCAS-SCHE>
2015-03-22 04:56:59 -0300

    a.

--- test/reverseJ/RecorderTest.java
+++ test/reverseJ/RecorderTest.java
@@ -39,7 +39,7 @@ public class RecorderTest{
 			assertArrayEquals(expectedAll, actual);
 		}
 	}
-	public static class PublicMethod extends Log{
+	public static class Method extends Log{
 		Actor actor;
 		String expected;
 		@Before
@@ -50,7 +50,7 @@ public class RecorderTest{
 		@Test
 		public void caller() throws Exception{
 			actor.play();
-			expected ="caller : reverseJ.RecorderTest.PublicMethod";
+			expected ="caller : reverseJ.RecorderTest.Method";
 			String[] actual = this.describeAll();
 			assertEquals(expected, actual[0]);
 		}
@@ -90,8 +90,69 @@ public class RecorderTest{
 			assertEquals(expected, actual[4]);
 		}
 	}
+	public static class MethodPrivate extends Log{
+		Actor actor;
+		String expected;
+		@Before
+		public void setup(){
+			actor = new Actor();
+			Recorder.determineLog(this);
+		}
+		@Test
+		public void caller() throws Exception{
+			actor.playPrivate();
+			expected ="caller : reverseJ.RecorderTest.MethodPrivate";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[0]);
+		}
+		@Test
+		public void target() throws Exception{
+			actor.playPrivate();
+			expected ="target : reverseJ.RecorderTest.Actor";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[1]);
+		}
+		@Test
+		public void method() throws Exception{
+			actor.playPrivate();
+			expected ="method : access$0";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[2]);
+		}
+		@Test
+		public void signature_NoParameter() throws Exception{
+			actor.playPrivate();
+			expected ="signature : ()";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[3]);
+		}
+		@Test
+		public void signature_OneParameter() throws Exception{
+			actor.playPrivate(true);
+			expected ="signature : (reverseJ.RecorderTest.Actor arg0, boolean arg1)";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[3]);
+		}
+		@Test
+		public void signature_TwoParameters() throws Exception{
+			actor.playPrivate(1, "a");
+			expected ="signature : (reverseJ.RecorderTest.Actor arg0, int arg1, java.lang.String arg2)";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[3]);
+		}
+		@Test
+		public void signature_returnType() throws Exception{
+			actor.playPrivate(1, "a");
+			expected ="return : java.lang.Boolean";
+			String[] actual = this.describeAll();
+			assertEquals(expected, actual[4]);
+		}
+	}
 	static class Actor{
 		static void play(){}
 		static boolean play(int i, String s){return true;}
+		private static void playPrivate(){}
+		private boolean playPrivate(int i, String s){return true;}
+		private boolean playPrivate(boolean b){return b;}
 	}
 }
