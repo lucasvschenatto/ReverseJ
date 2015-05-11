35df309a9265148886f69354002b08bc8c363d25
Lucas <Lucas@LUCAS-SCHE>
2015-05-11 07:48:38 -0300

    progredindo com os diagramas

--- src/reverseJ/DiagramMaker.java
+++ src/reverseJ/DiagramMaker.java
@@ -4,357 +4,127 @@ import java.io.File;
 import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.uml2.common.util.UML2Util;
 import org.eclipse.uml2.uml.AggregationKind;
 import org.eclipse.uml2.uml.Association;
+import org.eclipse.uml2.uml.BehaviorExecutionSpecification;
 import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.CombinedFragment;
 import org.eclipse.uml2.uml.Enumeration;
 import org.eclipse.uml2.uml.EnumerationLiteral;
+import org.eclipse.uml2.uml.ControlFlow;
+import org.eclipse.uml2.uml.ExecutionEnvironment;
+import org.eclipse.uml2.uml.ExecutionOccurrenceSpecification;
+import org.eclipse.uml2.uml.ExecutionSpecification;
 import org.eclipse.uml2.uml.Generalization;
+import org.eclipse.uml2.uml.Lifeline;
 import org.eclipse.uml2.uml.LiteralUnlimitedNatural;
+import org.eclipse.uml2.uml.Message;
+import org.eclipse.uml2.uml.MessageOccurrenceSpecification;
 import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.OccurrenceSpecification;
 import org.eclipse.uml2.uml.PrimitiveType;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.Type;
 import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.resource.UMLResource;
 import org.eclipse.uml2.uml.resources.util.UMLResourcesUtil;
+import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.Interaction;
 
 public class DiagramMaker {
 
 //	public static boolean DEBUG = true;
 
 	private static File outputDir;
-
+	private static ResourceSet resourceSet = new ResourceSetImpl();
 	public static void main(String[] args) throws Exception{
-		Model myModel = createModel("sequenceTest");
-//		outputDir = new File().getCanonicalFile();
-		URI outputURI = URI.createFileURI("../ReverseJ/files/ExtendedPO2")
-//		.appendSegment("files")
-//		.appendSegment("ExtendedPO2")
+		Package myPackage = createPackage("packageSequenceGoal");
+		Interaction myInteraction = createInteraction(myPackage, "interactionTest1");
+		Lifeline caller = createLifeLine(myInteraction, "caller");
+		Lifeline target = createLifeLine(myInteraction, "target");
+		
+		Message m = myInteraction.createMessage("talk");
+		
+		MessageOccurrenceSpecification invStart1 = UMLFactory.eINSTANCE.createMessageOccurrenceSpecification();
+		invStart1.setEnclosingInteraction(myInteraction);
+		invStart1.setName("invocationStart1");
+		invStart1.setMessage(m);
+		invStart1.setCovered(caller);
+		MessageOccurrenceSpecification invFinish1 = UMLFactory.eINSTANCE.createMessageOccurrenceSpecification();
+		invFinish1.setEnclosingInteraction(myInteraction);
+		invFinish1.setName("invocationFinish1");
+		invFinish1.setCovered(caller);
+		BehaviorExecutionSpecification invBody1 = UMLFactory.eINSTANCE.createBehaviorExecutionSpecification();
+		invBody1.setEnclosingInteraction(myInteraction);
+		invBody1.setName("invocationBody1");
+		invBody1.setStart(invStart1);
+		invBody1.setFinish(invFinish1);
+		
+		
+		MessageOccurrenceSpecification execStart1 = UMLFactory.eINSTANCE.createMessageOccurrenceSpecification();
+		execStart1.setEnclosingInteraction(myInteraction);
+		execStart1.setName("executionStart1");
+		execStart1.setMessage(m);
+		execStart1.setCovered(target);
+		MessageOccurrenceSpecification execFinish1 = UMLFactory.eINSTANCE.createMessageOccurrenceSpecification();
+		execFinish1.setEnclosingInteraction(myInteraction);
+		execFinish1.setName("executionFinish1");
+		execFinish1.setCovered(target);
+		BehaviorExecutionSpecification execBody1 = UMLFactory.eINSTANCE.createBehaviorExecutionSpecification();
+		execBody1.setEnclosingInteraction(myInteraction);
+		execBody1.setName("executionBody1");
+		execBody1.setStart(execStart1);
+		execBody1.setFinish(execFinish1);
+		ExecutionOccurrenceSpecification a = UMLFactory.eINSTANCE.createExecutionOccurrenceSpecification();
+		a.setEnclosingInteraction(myInteraction);
+		a.setName("executionBody2");
+		a.setExecution(execBody1);
+		a.setCovered(target);
+		
+		
+		
+		m.setSendEvent(invStart1);
+		m.setReceiveEvent(execStart1);
+		m.setInteraction(myInteraction);
+		
+		
+		
+//		ExecutionSpecification eS = UMLFactory.eINSTANCE.createExecutionOccurrenceSpecification().getExecution();
+//		myInteraction.createFragment("invocation-1-start", eS.eClass());
+//		UMLPackage uPackage = UMLFactory.eINSTANCE.createMessage();
+//		MessageEnd mE = org.eclipse.uml2.uml.UMLPackage.getMessageEnd();
+		
+//		Model myModel = createModel("sequenceTest");
+		URI outputURI = URI.createFileURI("../ReverseJ/files/diagramStudy")
 		.appendFileExtension(UMLResource.FILE_EXTENSION);
-		save(myModel, outputURI);
+		save(myPackage, outputURI);
 	} 
-
-	public static void oldMain(String[] args) throws Exception {
-
-		if (!processArgs(args)) {
-			System.exit(1);
-		}
-
-		banner("Creating root model package and primitive types.");
-
-		// Create the root package (a model).
-		Model epo2Model = createModel("epo2");
-
-		// Create primitive types to be used as types of attributes in our
-		// classes.
-		PrimitiveType intPrimitiveType = createPrimitiveType(epo2Model, "int");
-		PrimitiveType stringPrimitiveType = createPrimitiveType(epo2Model,
-				"String");
-		PrimitiveType datePrimitiveType = createPrimitiveType(epo2Model, "Date");
-		PrimitiveType skuPrimitiveType = createPrimitiveType(epo2Model, "SKU");
-
-		// Create enumerations to be used as types of attributes in our classes.
-		Enumeration orderStatusEnumeration = createEnumeration(epo2Model,
-				"OrderStatus");
-		createEnumerationLiteral(orderStatusEnumeration, "Pending");
-		createEnumerationLiteral(orderStatusEnumeration, "BackOrder");
-		createEnumerationLiteral(orderStatusEnumeration, "Complete");
-
-		banner("Creating model classes.");
-
-		// Create the classes.
-		org.eclipse.uml2.uml.Class supplierClass = createClass(epo2Model,
-				"Supplier", false);
-		org.eclipse.uml2.uml.Class customerClass = createClass(epo2Model,
-				"Customer", false);
-		org.eclipse.uml2.uml.Class purchaseOrderClass = createClass(epo2Model,
-				"PurchaseOrder", false);
-		org.eclipse.uml2.uml.Class itemClass = createClass(epo2Model, "Item",
-				false);
-		org.eclipse.uml2.uml.Class addressClass = createClass(epo2Model,
-				"Address", true);
-		org.eclipse.uml2.uml.Class usAddressClass = createClass(epo2Model,
-				"USAddress", false);
-		org.eclipse.uml2.uml.Class globalAddressClass = createClass(epo2Model,
-				"GlobalAddress", false);
-		org.eclipse.uml2.uml.Class globalLocationClass = createClass(epo2Model,
-				"GlobalLocation", false);
-
-		// Create generalization relationships amongst our classes.
-		createGeneralization(usAddressClass, addressClass);
-		createGeneralization(globalAddressClass, addressClass);
-		createGeneralization(globalAddressClass, globalLocationClass);
-
-		banner("Creating attributes of model classes.");
-
-		// Create attributes in our classes.
-		createAttribute(supplierClass, "name", stringPrimitiveType, 0, 1);
-		createAttribute(customerClass, "customerID", intPrimitiveType, 1, 1);
-		createAttribute(purchaseOrderClass, "comment", stringPrimitiveType, 0,
-				1);
-		createAttribute(purchaseOrderClass, "orderDate", datePrimitiveType, 1,
-				1);
-		createAttribute(purchaseOrderClass, "status", orderStatusEnumeration,
-				1, 1);
-		createAttribute(purchaseOrderClass, "totalAmount", intPrimitiveType, 0,
-				1);
-		createAttribute(itemClass, "productName", stringPrimitiveType, 0, 1);
-		createAttribute(itemClass, "quantity", intPrimitiveType, 0, 1);
-		createAttribute(itemClass, "usPrice", intPrimitiveType, 0, 1);
-		createAttribute(itemClass, "comment", stringPrimitiveType, 0, 1);
-		createAttribute(itemClass, "shipDate", datePrimitiveType, 0, 1);
-		createAttribute(itemClass, "partNum", skuPrimitiveType, 1, 1);
-		createAttribute(addressClass, "name", stringPrimitiveType, 0, 1);
-		createAttribute(addressClass, "country", stringPrimitiveType, 0, 1);
-		createAttribute(usAddressClass, "street", stringPrimitiveType, 1, 1);
-		createAttribute(usAddressClass, "city", stringPrimitiveType, 1, 1);
-		createAttribute(usAddressClass, "state", stringPrimitiveType, 1, 1);
-		createAttribute(usAddressClass, "zip", stringPrimitiveType, 1, 1);
-		createAttribute(globalAddressClass, "location", stringPrimitiveType, 1,
-				1);
-		createAttribute(globalLocationClass, "countryCode", intPrimitiveType,
-				1, 1);
-
-		banner("Creating associations between model classes.");
-
-		// Create associations between our classes.
-		createAssociation(supplierClass, true,
-				AggregationKind.COMPOSITE_LITERAL, "orders", 0,
-				LiteralUnlimitedNatural.UNLIMITED, purchaseOrderClass, false,
-				AggregationKind.NONE_LITERAL, "", 1, 1);
-		createAssociation(supplierClass, true, AggregationKind.NONE_LITERAL,
-				"pendingOrders", 0, LiteralUnlimitedNatural.UNLIMITED,
-				purchaseOrderClass, false, AggregationKind.NONE_LITERAL, "", 0,
-				1);
-		createAssociation(supplierClass, true, AggregationKind.NONE_LITERAL,
-				"shippedOrders", 0, LiteralUnlimitedNatural.UNLIMITED,
-				purchaseOrderClass, false, AggregationKind.NONE_LITERAL, "", 0,
-				1);
-		createAssociation(supplierClass, true,
-				AggregationKind.COMPOSITE_LITERAL, "customers", 0,
-				LiteralUnlimitedNatural.UNLIMITED, customerClass, false,
-				AggregationKind.NONE_LITERAL, "", 1, 1);
-		createAssociation(customerClass, true, AggregationKind.NONE_LITERAL,
-				"orders", 0, LiteralUnlimitedNatural.UNLIMITED,
-				purchaseOrderClass, true, AggregationKind.NONE_LITERAL,
-				"customer", 1, 1);
-		createAssociation(purchaseOrderClass, true,
-				AggregationKind.NONE_LITERAL, "previousOrder", 0, 1,
-				purchaseOrderClass, false, AggregationKind.NONE_LITERAL, "", 0,
-				1);
-		createAssociation(purchaseOrderClass, true,
-				AggregationKind.COMPOSITE_LITERAL, "items", 0,
-				LiteralUnlimitedNatural.UNLIMITED, itemClass, true,
-				AggregationKind.NONE_LITERAL, "order", 1, 1);
-		createAssociation(purchaseOrderClass, true,
-				AggregationKind.COMPOSITE_LITERAL, "billTo", 1, 1,
-				addressClass, false, AggregationKind.NONE_LITERAL, "", 1, 1);
-		createAssociation(purchaseOrderClass, true,
-				AggregationKind.COMPOSITE_LITERAL, "shipTo", 1, 1,
-				addressClass, false, AggregationKind.NONE_LITERAL, "", 1, 1);
-
-		// Save our model to a file in the user-specified output directory
-		URI outputURI = URI.createFileURI(outputDir.getAbsolutePath())
-				.appendSegment("ExtendedPO2")
-				.appendFileExtension(UMLResource.FILE_EXTENSION);
-		banner("Saving the model to %s.", outputURI.toFileString());
-		save(epo2Model, outputURI);
-	}
 
 	//
 	// Model-building utilities
 	//
-
-	protected static Model createModel(String name) {
-		Model model = UMLFactory.eINSTANCE.createModel();
-		model.setName(name);
-
-		out("Model '%s' created.", model.getQualifiedName());
-
-		return model;
+	private static Lifeline createLifeLine(Interaction interaction, String name){
+		Lifeline l = interaction.createLifeline(name);
+		return l;
 	}
-
-	protected static org.eclipse.uml2.uml.Package createPackage(
-			org.eclipse.uml2.uml.Package nestingPackage, String name) {
-
-		org.eclipse.uml2.uml.Package package_ = nestingPackage
-				.createNestedPackage(name);
-
-		out("Package '%s' created.", package_.getQualifiedName());
-
-		return package_;
-	}
-
-	protected static PrimitiveType createPrimitiveType(
-			org.eclipse.uml2.uml.Package package_, String name) {
-
-		PrimitiveType primitiveType = package_.createOwnedPrimitiveType(name);
-
-		out("Primitive type '%s' created.", primitiveType.getQualifiedName());
-
-		return primitiveType;
-	}
-
-	protected static Enumeration createEnumeration(
-			org.eclipse.uml2.uml.Package package_, String name) {
-
-		Enumeration enumeration = package_.createOwnedEnumeration(name);
-		out("Enumeration '%s' created.", enumeration.getQualifiedName());
-
-		return enumeration;
-	}
-
-	protected static EnumerationLiteral createEnumerationLiteral(
-			Enumeration enumeration, String name) {
-
-		EnumerationLiteral enumerationLiteral = enumeration
-				.createOwnedLiteral(name);
-
-		out("Enumeration literal '%s' created.",
-				enumerationLiteral.getQualifiedName());
-
-		return enumerationLiteral;
-	}
-
-	protected static org.eclipse.uml2.uml.Class createClass(
-			org.eclipse.uml2.uml.Package package_, String name,
-			boolean isAbstract) {
-
-		org.eclipse.uml2.uml.Class class_ = package_.createOwnedClass(name,
-				isAbstract);
-
-		out("Class '%s' created.", class_.getQualifiedName());
-
-		return class_;
-	}
-
-	protected static Generalization createGeneralization(
-			Classifier specificClassifier, Classifier generalClassifier) {
-
-		Generalization generalization = specificClassifier
-				.createGeneralization(generalClassifier);
-
-		out("Generalization %s --|> %s created.",
-				specificClassifier.getQualifiedName(),
-				generalClassifier.getQualifiedName());
-
-		return generalization;
-	}
-
-	protected static Property createAttribute(
-			org.eclipse.uml2.uml.Class class_, String name, Type type,
-			int lowerBound, int upperBound) {
-
-		Property attribute = class_.createOwnedAttribute(name, type,
-				lowerBound, upperBound);
-
-		out("Attribute '%s' : %s [%s..%s] created.", //
-				attribute.getQualifiedName(), // attribute name
-				type.getQualifiedName(), // type name
-				lowerBound, // no special case for multiplicity lower bound
-				(upperBound == LiteralUnlimitedNatural.UNLIMITED) ? "*" // special
-																		// case
-																		// for
-																		// unlimited
-																		// bound
-						: upperBound);
-
-		return attribute;
-	}
-
-	protected static Association createAssociation(Type type1,
-			boolean end1IsNavigable, AggregationKind end1Aggregation,
-			String end1Name, int end1LowerBound, int end1UpperBound,
-			Type type2, boolean end2IsNavigable,
-			AggregationKind end2Aggregation, String end2Name,
-			int end2LowerBound, int end2UpperBound) {
-
-		Association association = type1.createAssociation(end1IsNavigable,
-				end1Aggregation, end1Name, end1LowerBound, end1UpperBound,
-				type2, end2IsNavigable, end2Aggregation, end2Name,
-				end2LowerBound, end2UpperBound);
-
-		out("Association %s [%s..%s] %s-%s %s [%s..%s] created.", //
-				UML2Util.isEmpty(end1Name)
-				// compute a placeholder for the name
-				? String.format("{%s}", type1.getQualifiedName()) //
-						// user-specified name
-						: String.format("'%s::%s'", type1.getQualifiedName(),
-								end1Name), //
-				end1LowerBound, // no special case for this
-				(end1UpperBound == LiteralUnlimitedNatural.UNLIMITED) ? "*" // special
-																			// case
-																			// for
-																			// unlimited
-																			// upper
-																			// bound
-						: end1UpperBound, // finite upper bound
-				end2IsNavigable ? "<" // indicate navigability
-						: "-", // not navigable
-				end1IsNavigable ? ">" // indicate navigability
-						: "-", // not navigable
-				UML2Util.isEmpty(end2Name)
-				// compute a placeholder for the name
-				? String.format("{%s}", type2.getQualifiedName()) //
-						// user-specified name
-						: String.format("'%s::%s'", type2.getQualifiedName(),
-								end2Name), //
-				end2LowerBound, // no special case for this
-				(end2UpperBound == LiteralUnlimitedNatural.UNLIMITED) ? "*" // special
-																			// case
-																			// for
-																			// unlimited
-																			// upper
-																			// bound
-						: end2UpperBound);
-
-		return association;
-	}
-
-	//
-	// Program control
-	//
-
-	private static boolean processArgs(String[] args) throws IOException {
-
-		if (args.length != 1) {
-			err("Expected 1 argument.");
-			err("Usage: java -jar ... %s <dir>",
-					DiagramMaker.class.getSimpleName());
-			err("where");
-			err("<dir> - path to output folder in which to save the UML model");
-			return false;
+	private static Interaction createInteraction(Package package_, String name){
+		Interaction i = UMLFactory.eINSTANCE.createInteraction();
+		i.setName(name);
+		i.setPackage(package_);
+		return i;
 		}
-
-		outputDir = new File(args[0]).getCanonicalFile();
-		if (!outputDir.exists()) {
-			err("No such directory: %s", outputDir.getAbsolutePath());
-			return false;
-		}
-
-		if (!outputDir.isDirectory()) {
-			err("Not a directory: %s", outputDir.getAbsolutePath());
-			return false;
-		}
-
-		if (!outputDir.canWrite()) {
-			err("Cannot create a file in directory: %s",
-					outputDir.getAbsolutePath());
-			return false;
-		}
-
-		return true;
+	private static Package createPackage(String name){
+		Package p = UMLFactory.eINSTANCE.createPackage();
+		p.setName(name);
+		return p;
 	}
-
-	protected static void save(org.eclipse.uml2.uml.Package package_, URI uri) {
+	private static void save(org.eclipse.uml2.uml.Package package_, URI uri) {
 		// Create a resource-set to contain the resource(s) that we are saving
-		ResourceSet resourceSet = new ResourceSetImpl();
-
 		// Initialize registrations of resource factories, library models,
 		// profiles, Ecore metadata, and other dependencies required for
 		// serializing and working with UML resources. This is only necessary in
@@ -370,46 +140,13 @@ public class DiagramMaker {
 		// And save
 		try {
 			resource.save(null); // no save options needed
-			out("Done.");
 		} catch (IOException ioe) {
 			err(ioe.getMessage());
 		}
 	}
-
-	//
-	// Logging utilities
-	//
-
-	protected static void banner(String format, Object... args) {
-		System.out.println();
-		hrule();
-
-		System.out.printf(format, args);
-		if (!format.endsWith("%n")) {
-			System.out.println();
-		}
-
-		hrule();
-		System.out.println();
-	}
-
-	protected static void hrule() {
-		System.out.println("------------------------------------");
-	}
-
-	protected static void out(String format, Object... args) {
-//		if (DEBUG) {
-//			System.out.printf(format, args);
-//			if (!format.endsWith("%n")) {
-//				System.out.println();
-//			}
-//		}
-	}
-
-	protected static void err(String format, Object... args) {
+	private static void err(String format, Object... args) {
 		System.err.printf(format, args);
-		if (!format.endsWith("%n")) {
+		if (!format.endsWith("%n"))
 			System.err.println();
-		}
 	}
 }
