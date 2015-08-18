e3bebad2d139adf6159821850f4b7937787f6ac4
Lucas <Lucas@LucasSche>
2015-08-17 23:52:51 -0300

    a

---
+++ test/acceptanceTests/StoryClassAndSequenceDiagramTest.java
@@ -0,0 +1,38 @@
+package acceptanceTests;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import reverseJ.ClassDiagram;
+import reverseJ.DiagramMaker;
+import reverseJ.DiagramStrategy;
+import reverseJ.InformationProvider;
+import reverseJ.InformationStorageProvider;
+import reverseJ.MakerAndSaver;
+import reverseJ.RecorderStorage;
+import reverseJ.SequenceDiagram;
+import reverseJ.Tracer;
+import StoryPackage.Story;
+
+public class StoryClassAndSequenceDiagramTest {
+	public static void main(String[] args) {
+		InformationStorageProvider i = new InformationStorageProvider();
+		RecorderStorage r = i;
+		InformationProvider p = i;
+		List<DiagramStrategy> lds = new LinkedList<DiagramStrategy>();
+		lds.add(new SequenceDiagram());
+		lds.add(new ClassDiagram());
+		DiagramMaker dM = new MakerAndSaver(p, lds);
+		((MakerAndSaver)dM).setFileName("classeESequencia");
+		Tracer.start(r);
+		
+		Story s = new Story();
+		s.tellStory();
+		
+		Tracer.stop();
+		for (String info : r.describeAll()) {
+			System.out.println(info);
+		}
+		dM.make();
+	}
+}
