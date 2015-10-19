7f147f6139c7b41f542ad49f4267893ed668b261
Lucas <Lucas@LucasSche>
2015-10-19 19:48:23 -0200

    a little more decoupled

--- src/reverseJ/InformationFactory.java
+++ src/reverseJ/InformationFactory.java
@@ -1,37 +1,37 @@
 package reverseJ;
 
 public abstract class InformationFactory {
-	public static IClass createClass(String value){
+	public static Information createClass(String value){
 		return new IClass(value);
 	}
-	public static ICaller createCaller(String value){
+	public static Information createCaller(String value){
 		return new ICaller(value);
 	}
-	public static IInterface createInterface(String value) {
+	public static Information createInterface(String value) {
 		return new IInterface(value);
 	}
-	public static ITarget createTarget(String value) {
+	public static Information createTarget(String value) {
 		return new ITarget(value);
 	}
-	public static IModifiers createModifiers(String value) {
+	public static Information createModifiers(String value) {
 		return new IModifiers(value);
 	}
-	public static IMethod createMethod(String value) {
+	public static Information createMethod(String value) {
 		return new IMethod(value);
 	}
-	public static IParameters createParameters(String value) {
+	public static Information createParameters(String value) {
 		return new IParameters(value);
 	}
-	public static IReturn createReturn(String value) {
+	public static Information createReturn(String value) {
 		return new IReturn(value);
 	}
-	public static IThrow createThrow(String value) {
+	public static Information createThrow(String value) {
 		return new IThrow(value);
 	}
-	public static IHandler createHandler(String value) {
+	public static Information createHandler(String value) {
 		return new IHandler(value);
 	}
-	public static IDummy createDummy(String value) {
+	public static Information createDummy(String value) {
 		return new IDummy(value);
 	}
 }
